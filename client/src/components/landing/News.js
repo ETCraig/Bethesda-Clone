88228009e7d0641f533b237d2d1f4bee7949aecb
unknown <devethancraig@gmail.com>
2019-01-18 15:43:11 -0700

    Making Progress on News.js

--- client/src/components/landing/News.js
+++ client/src/components/landing/News.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Arrow from '../../assets/icons8-next-page-24.png';
 import styled from 'styled-components';
 import Img from '../../assets/Large-Hero.jpg';
+import Paragraph from '../../assets/icons8-align-left-24.png';
+
+const NewsWrapper = styled.div`
+    width: 100%;
+    min-height: 45vw;
+    display: block;
+`;
+
+const DragonContainer = styled.div`
+    width: 66%;
+    float: left;
+    height: 100%;
+    position: relative;
+`;
 
 const ImageDragon = styled.img`
     vertical-align: middle;
@@ -10,43 +25,156 @@ const ImageDragon = styled.img`
     height: 691.2px;
     width: 65.9vw;
     max-width: 100%;
-    display: inline-block;
+    display: initial;
 `;
 
-const BlackRight = styled.div`
-    text-align: center;
-    word-spacing: 0px;
-    background-position: 0% 0%;
-    height: 691.2px;
-    width: 33vw;
-    position: relative;
-    top: 0px;
-    bottom: 0px;
-    right: 0px;
-    left: 0px;
+const NewsRight = styled.div`
+    height: 100%;
     display: flex;
+    position: relative;
+    align-items: center;
+    align-content: center;
     background-color: black;
+    height: 691.2px
+    box-sizing: border-box;
+    padding: 3em 2em 2em;
 `;
 
 const InnerWrapper = styled.div`
-    height: 577.4px;
-    width: 501.325px;
-    padding: 48px 32px 32px 32px;
-    min-height: auto;
-    min-width: auto;
+    padding: 3em 2em 2em;
+    box-sizing: border-box;
+    font-size: 16px;
+    font-family: Montserrat;
+    font-weight: 400;
+    line-height: 1.63;
+    letter-spacing: 0;
+`;
+
+const H2News = styled.h2`
+    color: rgb(255, 255, 255);
+    font-size: 2.3em;
+    display: inline-block;
+    margin-left: -25%;
+    margin-bottom: 30px;
+    font-weight: 700;
+    font-family: Trade Gothic LT;
+    line-height: 1.06;
+`;
+
+const NewsContainer = styled.div`
     display: block;
+    margin-left: -5px;
+    align-items: center;
+    justify-content: center;
+    color: rgb(255, 255, 255);
+    box-sizing: border-box;
+`;
+
+const NewsPar = styled.img`
+    width: 18px;
+    height: 18px;
+    vertical-align: middle;
+    display: inline-block;
+    font-size: 24px;
+    transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    user-select: none;
+    flex-shrink: 0;
+`;
+
+const NewsEos = styled.span`
+    font-size: .63em;
+    font-family: Trade Gothic LT Extended;
+    font-weight: 700;
+    line-height: 1.63;
+    letter-spacing: .1em;
+    text-transform: uppercase;
+    vertical-align: middle;
+    color: rgb(255, 255, 255);
+`;
+
+const EosCont = styled.div`
+    font-size: 14px;
+    margin-top: 20px;
+    font-weight: 400;
+    box-sizing: border-box;
+`;
+
+const Jan15 = styled.span`
+    font-size: 10px;
+    font-weight: 300;
+    padding: 0;
+    margin-top: 10px;
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 0;
+    vertical-align: middle;
+    text-transform: uppercase
+`;
+
+const ReadMoreCont = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const ReadMoreInner = styled.div`
+    margin-top: 30px;
+    margin-bottom: 30px;
+    display: block;
+`;
+
+const ArrowIcon = styled.img`
+    fill: #202020;
+    border-radius: 1em;
+    vertical-align: middle;
+    background-color: #F8F8F8;
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+    font-size: 24px;
+    transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    user-select: none;
+    flex-shrink: 0;
+`;
+
+const ReadMore = styled.span`
+    font-size: .75em;
+    font-family: Trade Gothic LT Extended;
+    font-weight: 700;
+    line-height: 1.75;
+    letter-spacing: .1em;
+    text-transform: uppercase;
+    color: #fff;
+    box-sizing: border-box;
+    margin-left: 1em;
 `;
 
 const News = props => {
     return(
-    <div style={{display: 'flex', flexDirection: 'row'}}>
-        <ImageDragon src={Img} />
-        <BlackRight>
-            <InnerWrapper>
+    <NewsWrapper>
+        <DragonContainer>
+            <ImageDragon src={Img} />
+        </DragonContainer>
 
+        <NewsRight>
+            <InnerWrapper>
+                <a><H2News>ANNOUNCING ESO: ELSWEYR & THE SEASON OF THE DRAGON</H2News></a>
+                <NewsPar src={Paragraph} /><NewsEos>News</NewsEos>
+                <EosCont>
+                    "We are happy to announce the next Chapter in The Elder Scrolls Online saga, The Elder Scrolls Online: Elsweyr, coming to PC/Mac, Xbox One and PlayStation 4 on June 4. Learn about this exciting new adventure and the year-long Season of the Dragon storyline right here."
+                </EosCont>
+                <Jan15>The Elder Scrolls Online Elsweyr - Jan 15</Jan15>
+                <ReadMoreCont>
+                    <a>
+                        <ReadMoreInner>
+                            <ArrowIcon src={Arrow} /><ReadMore>Read More</ReadMore>
+                        </ReadMoreInner>
+                    </a>
+                </ReadMoreCont>
             </InnerWrapper>
-        </BlackRight>
-    </div>
+        </NewsRight>
+    </NewsWrapper>
     )
 }
 
