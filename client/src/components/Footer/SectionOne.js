98d6642ae17b3e348c48f51849e5c9871efc8301
unknown <devethancraig@gmail.com>
2019-01-22 11:59:21 -0700

    SectionTwo Created

--- client/src/components/Footer/SectionOne.js
+++ client/src/components/Footer/SectionOne.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-// import Globe from '../../assets/'
+import Download from '../../assets/icons8-download-24.png';
+import Globe from '../../assets/icons8-geography-24.png';
 import styled from 'styled-components';
 
 const SectionOneWrapper = styled.div`
@@ -74,21 +75,6 @@ const SpacingDiv = styled.div`
     display: block;
 `;
 
-const LauncherWrapper = styled.div`
-    flex: 1 1 auto;
-    margin: 0.625rem;
-    height: 14.63rem;
-    display: flex;
-    padding: 1.25rem 0;
-    position: relative;
-    text-align: center;
-    min-height: 14.63rem;
-    align-items: center;
-    flex-direction: column;
-    justify-content: space-between;
-    box-sizing: border-box;
-`;
-
  const LanguageBtnWrapper = styled.div`
     box-sizing: border-box;
     display: block;
@@ -134,12 +120,155 @@ const LanguageBtn = styled.button`
     overflow: visible;
 `;
 
+const LauncherWrapper = styled.div`
+    flex: 1 1 auto;
+    margin: 0.625rem;
+    height: 14.63rem;
+    display: flex;
+    padding: 1.25rem 0;
+    position: relative;
+    text-align: center;
+    min-height: 14.63rem;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+    box-sizing: border-box;
+`;
+
+const ImageWrapper = styled.div`
+    margin: 0 0 0.625rem 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    box-sizing: border-box;
+`;
+
+const DownloadImage = styled.img`
+    fill: currentColor;
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+    font-size: 24px;
+    transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    user-select: none;
+    flex-shrink: 0;
+    box-sizing: border-box;
+    overflow: hidden;
+`;
+
+const DownloadText = styled.p`
+    font-family: Montserrat,sans-serif;
+    font-size: 1em;
+    font-weight: 600;
+    padding-top: 10px;
+    line-height: 1.4;
+    list-style-position: outside;
+    margin: 0 0 15px;
+    padding: 0;
+    box-sizing: border-box;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+`;
+
+const DescriptionWrapper = styled.div`
+    padding-bottom: 1rem;
+    box-sizing: border-box;
+    display: block;
+`;
+
+const DescriptionText = styled.p`
+    font-family: Montserrat,sans-serif;
+    color: #b3b3b3;
+    margin: 0;
+    padding: 0 1rem;
+    font-size: 0.6em;
+    font-weight: 600;
+    text-transform: uppercase;
+    line-height: 1.4;
+    list-style-position: outside;
+    box-sizing: border-box;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    text-align: center;
+`;
+
+const ButtonWrapper = styled.div`
+box-sizing: border-box;
+display: block;
+text-align: center;
+`;
+
+const DownloadButton = styled.button`
+color: #fff;
+    border: 1px solid #fff;
+    height: 5.5em;
+    min-width: 26em;
+    box-shadow: none;
+    line-height: 100%;
+    border-radius: 2px;
+    text-transform: uppercase;
+    background-color: transparent;
+    font-size: .625em;
+    padding: 21px 60px
+    font-family: Trade Gothic LT Extended;
+    letter-spacing: 1px;
+    box-sizing: border-box;
+    min-height: 36px;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    font-weight: 500;
+    margin: 0;
+    cursor: pointer;
+    display: inline-flex;
+    outline: none;
+    position: relative;
+    align-items: center;
+    user-select: none;
+    vertical-align: middle;
+    justify-content: center;
+    text-decoration: none;
+    -webkit-appearance: none;
+    -webkit-tap-highlight-color: transparent;
+`;
+
+const ButtonSpan = styled.span`
+    width: 100%;
+    display: inherit;
+    align-items: inherit;
+    justify-content: inherit;
+    box-sizing: border-box;
+`;
+
+const DownloadButtonText = styled.span`
+    box-sizing: border-box;
+`;
+
+const BtnBottomSpan = styled.span`
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: block;
+    z-index: 0;
+    position: absolute;
+    overflow: hidden;
+    border-radius: inherit;
+    pointer-events: none;
+    box-sizing: border-box;
+`;
+
 const SectionOne = props => {
     return(
         <SectionOneWrapper>
             <LanguageWrapper>
                 <LanguageContainer>
-                    {/* <GlobeImg src={Globe} /> */}
+                    <GlobeImg src={Globe} />
                     <LanguageText></LanguageText>
                 </LanguageContainer>
                 <SpacingDiv></SpacingDiv>
@@ -150,7 +279,23 @@ const SectionOne = props => {
                 </LanguageBtnWrapper>
             </LanguageWrapper>
             <LauncherWrapper>
-
+                <ImageWrapper>
+                    <DownloadImage src={Download} />
+                    <DownloadText>Bethesda Launcher</DownloadText>
+                </ImageWrapper>
+                <DescriptionWrapper>
+                    <DescriptionText>OS: Windows 7 / 8 / 8.1 / 10</DescriptionText>
+                    <DescriptionText>Processor: 2.5Ghz Intel Pentium 4 / AMD Athlon 64</DescriptionText>
+                    <DescriptionText>RAM: 1GB, Disk Space: 250MB</DescriptionText>
+                </DescriptionWrapper>
+                <ButtonWrapper>
+                    <DownloadButton>
+                        <ButtonSpan>
+                            <DownloadButtonText>Download</DownloadButtonText>
+                        </ButtonSpan>
+                        <BtnBottomSpan></BtnBottomSpan>
+                    </DownloadButton>
+                </ButtonWrapper>
             </LauncherWrapper>
         </SectionOneWrapper>
     );
--- client/src/components/Footer/SectionTwo.js
+++ client/src/components/Footer/SectionTwo.js
@@ -2,11 +2,142 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const SectionTwoWrapper = styled.div`
+    display: flex;
+    padding: 4vh 5vw;
+    justify-content: space-between;
+    background-color: rgb(29, 29, 29);
+    box-sizing: border-box;
+`;
+
+const ExploreWrapper = styled.div`
+    flex: 1;
+    width: 100%;
+    max-width: 25vw;
+    box-sizing: border-box;
+    display: block;
+`;
+
+const TitleWrapper = styled.div`
+    border-bottom: 2px solid rgb(46, 46, 46);
+    display: flex;
+    align-items: center;
+    margin-bottom: 1em;
+    box-sizing: border-box;
+`;
+
+const Title = styled.h5`
+    color: #fff;
+    margin: 1em 0;
+    font-size: 12px;
+    font-family: Trade Gothic LT Extended;
+    line-height: 1.4em;
+    letter-spacing: 1.5px;
+    font-weight: 700;
+    text-transform: uppercase;
+    padding: 0;
+    box-sizing: border-box;
+    display: block;
+    margin-block-start: 1.67em;
+    margin-block-end: 1.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+`;
+
+const ListWrapper = styled.ul`
+    padding: 0;
+    list-style: none;
+    margin-left: 0;
+    line-height: 1rem;
+    column-count: 2;
+    font-family: inherit;
+    font-size: 1em;
+    margin: 0 0 15px;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+`;
+
+const ListItems = styled.li`
+    margin-bottom: .75rem;
+    box-sizing: border-box;
+    display: list-item;
+    text-align: -webkit-match-parent;
+`;
+
+const ListLinks = styled.a`
+    color: #b3b3b3;
+    font-size: .75rem;
+    font-weight: 400;
+    text-decoration: none;
+    background-color: transparent;
+    box-sizing: border-box;
+`;
+
+const SupportWrapper = styled.div`
+    flex: 1;
+    width: 100%;
+    max-width: 25vw;
+    box-sizing: border-box;
+    display: block;
+`;
+
+
+
+
+const LegalWrapper = styled.div`
+    flex: 1;
+    width: 100%;
+    max-width: 25vw;
+    box-sizing: border-box;
+    display: block;
+`;
+
 const SectionTwo = props => {
     return(
-        <div>
-
-        </div>
+        <SectionTwoWrapper>
+            <ExploreWrapper>
+                <TitleWrapper>
+                    <Title>Explore</Title>
+                </TitleWrapper>
+                <ListWrapper>
+                    <ListItems><ListLinks>Home</ListLinks></ListItems>
+                    <ListItems><ListLinks>Games</ListLinks></ListItems>
+                    <ListItems><ListLinks>Mods</ListLinks></ListItems>
+                    <ListItems><ListLinks>Community</ListLinks></ListItems>
+                    <ListItems><ListLinks>News</ListLinks></ListItems>
+                    <ListItems><ListLinks>Studios</ListLinks></ListItems>
+                    <ListItems><ListLinks>Playtest</ListLinks></ListItems>
+                </ListWrapper>
+            </ExploreWrapper>
+            <SupportWrapper>
+                <TitleWrapper>
+                    <Title>Support</Title>
+                </TitleWrapper>
+                <ListWrapper>
+                    <ListItems><ListLinks>Support Center</ListLinks></ListItems>
+                    <ListItems><ListLinks>Manuals and Safety Docs</ListLinks></ListItems>
+                    <ListItems><ListLinks>Status</ListLinks></ListItems>
+                </ListWrapper>
+            </SupportWrapper>
+            <LegalWrapper>
+                <TitleWrapper>
+                    <Title>Legal</Title>
+                </TitleWrapper>
+                <ListWrapper>
+                    <ListItems><ListLinks>Terms Of Service</ListLinks></ListItems>
+                    <ListItems><ListLinks>End User License Agreements</ListLinks></ListItems>
+                    <ListItems><ListLinks>Privacy Policy</ListLinks></ListItems>
+                    <ListItems><ListLinks>Legal information</ListLinks></ListItems>
+                    <ListItems><ListLinks>Code Of Conduct</ListLinks></ListItems>
+                    <ListItems><ListLinks>Contact</ListLinks></ListItems>
+                    <ListItems><ListLinks>Corporate</ListLinks></ListItems>
+                </ListWrapper>
+            </LegalWrapper>
+        </SectionTwoWrapper>
     );
 }
 
